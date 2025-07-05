eb72481fcb60d557149fa6318eeccfa128a01017
Hannah Ziemons <hannah.ziemons@foholding.de>
2025-07-05 10:00:30 +0200

    try to fix mobile version

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,7 +9,7 @@ const HomePage = () => {
   return (
     <div>
       <section className="hero">
-        <div className="container" style={{ padding: '0 150px' }}>
+        <div className="container">
           <div style={{ 
             display: 'flex', 
             flexWrap: 'wrap', 
